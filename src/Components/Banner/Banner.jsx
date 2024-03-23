92130f3fbdb50f931a92359b817628fd8b8b2148
BaileyKH <hen.baileyk@gmail.com>
2024-03-22 20:05:50 -0700

    created/styled  reusable banner components

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,19 +1,19 @@
 import './Banner.css'
 
-export const Banner = () => {
+export const Banner = ({title, description, image}) => {
     return(
         <div className="banner">
         <div className="container">
           <div className="banner-container">
             <div className="text-container">
               <div className="text">
-                <h2>Burton Step-On</h2>
-                <p>The newest addition to the burton line is their one of a kind step-on bindings, making it easier than ever to shred the moutain</p>
+                <h2>{title}</h2>
+                <p>{description}</p>
                   <button>Shop now</button>
               </div>
             </div>
             <div className="img-side">
-              <img src="/src/assets/banner-images/step-on.png" alt="banner" />
+              <img src={image} alt={title} />
             </div>
           </div>
         </div>
