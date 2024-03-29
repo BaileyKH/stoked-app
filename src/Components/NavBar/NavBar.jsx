fc579f861df4df885d721a21abaf24a9ab2c62b1
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-03-29 11:48:05 -0700

    added navigation responsiveness

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { useCart } from '/src/CartContext.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faCartShopping, faBars } from '@fortawesome/free-solid-svg-icons'
 
 import './NavBar.css'
 
 export const NavBar = () => {
     const { cartCount } = useCart();
 
+    const [navExpanded, setNavExpanded] = useState(false);
+
     return(
         <nav className="navbar">
             <Link to="/" className="nav-logo">STOKED</Link>
-            <div className="nav-items">
-                <Link to="/productpage" className="links" >Product Page</Link>
-                <Link to="/categories" className="links" >Categories</Link>
+            <div className={`slideout-menu ${navExpanded ? 'open' : 'nav-items'}`} >
+                <Link to="/productpage" className="links" onClick={navExpanded ? 'close' : ''} >Product Page</Link>
+                <Link to="/categories" className="links" onClick={navExpanded ? 'close' : ''} >Categories</Link>
                 <div className="search-container">
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                     <input className="search-input" 
@@ -23,7 +25,7 @@ export const NavBar = () => {
                         placeholder="Search"/>
                 </div>
                 <div className="cart-container">
-                    <Link to="/cart">
+                    <Link to="/cart" onClick={navExpanded ? 'close' : ''}>
                     <div>
                         <FontAwesomeIcon icon={faCartShopping} className="cart-logo"/>
                         {cartCount > 0 &&
@@ -32,6 +34,9 @@ export const NavBar = () => {
                     </div>
                     </Link>
                 </div>
+            </div>
+            <div className="nav-bar-container" onClick={() => setNavExpanded(!navExpanded)}>
+                <FontAwesomeIcon icon={faBars} className="bar-icon"/>
             </div>
         </nav>
     );
